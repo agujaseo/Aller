01356981674b8eca7d6392a9aa36739915157474
agujaseo <uaeup2019@gmail.com>
2025-02-13 14:43:03 +0100

    5

--- allergy-tracker/src/config/firebase.js
+++ allergy-tracker/src/config/firebase.js
@@ -4,13 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  // TODO: Replace with your Firebase config
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id"
+  apiKey: "AIzaSyDRfqpZz5vMKjBjZwjqhbF0WHK0hXzMxnM",
+  authDomain: "allergy-tracker-9f9c5.firebaseapp.com",
+  projectId: "allergy-tracker-9f9c5",
+  storageBucket: "allergy-tracker-9f9c5.appspot.com",
+  messagingSenderId: "1093305670670",
+  appId: "1:1093305670670:web:3c9a4c1f3fa9d9c6c3b9c5"
 };
 
 const app = initializeApp(firebaseConfig);
